864beefb92be42b23eee4a56babd60b4cd212865
Brooks Travis <brooks.travis@gmail.com>
2014-06-29 02:18:40 -0500

    Adding support for saving MAC addresses as strings in the database, and specifying a default EUI dialect via settings.

---
+++ macaddress/__init__.py
@@ -0,0 +1,36 @@
+from django.conf import settings
+
+from netaddr import mac_eui48, mac_unix
+
+import importlib
+
+def default_dialect(eui_obj=None):
+    # Check to see if a default dialect class has been specified in settings, 
+    # using 'module.dialect_cls' string and use importlib and getattr to retrieve dialect class. 'module' is the module and 
+    # 'dialect_cls' is the class name of the custom dialect. The dialect must either be defined or imported by the module's 
+    # __init__.py if the module is a package.
+    if hasattr(settings, 'MACADDRESS_DEFAULT_DIALECT'):
+        module, dialect_cls = settings.MACADDRESS_DEFAULT_DIALECT.split('.')
+        dialect = getattr(importlib.import_module(module), dialect_cls, mac_eui48)
+        return dialect
+    else:
+        if eui_obj:
+            return eui_obj.dialect
+        else:
+            return mac_eui48
+
+def format_mac(eui_obj, dialect):
+    # Format a EUI instance as a string using the supplied dialect class, allowing custom string classes by 
+    # passing directly or as a string, a la 'module.dialect_cls', where 'module' is the module and 'dialect_cls'
+    # is the class name of the custom dialect. The dialect must either be defined or imported by the module's __init__.py if 
+    # the module is a package.
+    if not isinstance(dialect, mac_eui48):
+        if isinstance(dialect, str):
+            module, dialect_cls = dialect.split('.')
+            dialect = getattr(importlib.import_module(module), dialect_cls)
+    eui_obj.dialect = dialect
+    return str(eui_obj)
+
+class mac_linux(mac_unix):
+    """MAC format with zero-padded all upper-case hex and colon separated"""
+    word_fmt = '%.2X'
--- macaddress/fields.py
+++ macaddress/fields.py
@@ -1,49 +1,49 @@
 from django.core.exceptions import ValidationError
 from django.db import models
 
-from netaddr import EUI, AddrFormatError, mac_unix
+from netaddr import EUI, AddrFormatError
 
-from formfields import MACAddressField as MACAddressFormField
+from .formfields import MACAddressField as MACAddressFormField
 
-
-class mac_linux(mac_unix):
-    """MAC format with zero-padded all upper-case hex and colon separated"""
-    word_fmt = '%.2X'
-
-
+from . import default_dialect, format_mac
+                 
 class MACAddressField(models.Field):
     description = "A MAC address validated by netaddr.EUI"
     empty_strings_allowed = False
     __metaclass__ = models.SubfieldBase
-    dialect = mac_linux # a default dialect
-
-    @classmethod
-    def set_dialect(cls, new_dialect_clazz):
-        ''' Setting dialect for EUI (MAC addresses) globally to this Field
-        class.
-        Class new_dialect_clazz should (finally) extend
-        netaddr.strategy.eui48.mac_eui48.
-        '''
-        cls.dialect = new_dialect_clazz
-
+    
+    def __init__(self, integer=True, *args, **kwargs): # Custom __init__ to accept new "integer" boolean argument to support specification of string or integer storage. Defaults to True (for now).
+        self.integer = integer
+        if not self.integer: # If storing MAC address as string, set max_length to default (17) or use supplied kwarg value.
+            kwargs['max_length'] = kwargs.get('max_length', 17)
+        super(MACAddressField, self).__init__(*args, **kwargs)
+    
     def get_prep_value(self, value):
         if value is None:
             return None
         if not isinstance(value, EUI):
-            return int(EUI(value, dialect=MACAddressField.dialect))
-        return int(value)
-
+            value = EUI(value, dialect=default_dialect())
+            if self.integer:
+                return int(value)
+            return str(value)
+        value.dialect = default_dialect(self)
+        if self.integer:
+            return int(value)
+        return str(value)
+    
     def get_internal_type(self):
-        return "BigIntegerField"
-
+        if self.integer:
+            return 'BigIntegerField'
+        return 'CharField'
+            
     def to_python(self, value):
         if value is None:
             return value
         if isinstance(value, EUI):
-            value.dialect = MACAddressField.dialect
+            value.dialect = default_dialect(value)
             return value
         try:
-            return EUI(value, dialect=MACAddressField.dialect)
+            return EUI(value, dialect=default_dialect())
         except (TypeError, ValueError, AddrFormatError):
             raise ValidationError(
                 "This value must be a valid MAC address.")
