073d5d7fb7ec52a5025f3dd50180c53222f1f298
kra3 <the1.arun@gmail.com>
2014-06-30 18:14:39 +0530

    increasing minor version number with a fix which effects django-debug-toolbar integration

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 import os
 from setuptools import setup, find_packages
 
-version = "1.2.1"
+version = "1.2.2"
 
 def read(fname):
     return open(os.path.join(os.path.dirname(__file__), fname)).read()
