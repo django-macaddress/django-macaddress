8bc242c7330ce60ba1045c6fa380175b4a4d2a10
kra3 <the1.arun@gmail.com>
2015-08-18 15:39:14 +0530

    release 1.3.2 and python wheel builds

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 import os
 from setuptools import setup, find_packages
 
-version = "1.3.1"
+version = "1.3.2"
 
 def read(fname):
     return open(os.path.join(os.path.dirname(__file__), fname)).read()
