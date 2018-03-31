9120fdde5d2c15e8aab5180bb434abcf95dba877
Arun Karunagath <akarunagath@apple.com>
2018-04-01 01:08:04 +0200

    clean up tests

--- macaddress/tests/test_fields.py
+++ macaddress/tests/test_fields.py
@@ -8,24 +8,19 @@ from .models import NetworkThingy
 
 
 class MACAddressFieldTestCase(TestCase):
-
-    def setUp(self):
-        self.model = NetworkThingy
-
     def test_insert_valid_macaddress(self):
-        mac_example = '00:11:22:33:44:AA'
-        x = self.model()
-        x.mac = mac_example
+        mac_example = '00:11:22:33:44:aa'
+        x = NetworkThingy(mac=mac_example)
         x.save()
+        qm = NetworkThingy.objects
         self.assertEquals(x.mac, mac_example)
-        self.assertEquals(NetworkThingy.objects.get(mac=mac_example).mac,
-                mac_example)
-        self.assertEquals(NetworkThingy.objects.all().count(), 1)
+        self.assertEquals(qm.get(mac=mac_example).mac, mac_example)
+        self.assertEquals(qm.all().count(), 1)
 
     def test_insert_invalid_macaddress(self):
         invalid_mac = 'XX'
         with transaction.atomic():
-            x = self.model()
+            x = NetworkThingy()
             with self.assertRaises(ValidationError):
                 x.mac = invalid_mac
                 x.save()
