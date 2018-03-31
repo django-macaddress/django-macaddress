0557f9594af5fb6de9b20d05867e0da50e5b9758
Arun Karunagath <akarunagath@apple.com>
2018-04-01 01:10:46 +0200

    bump minor version - supports django>=2

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 import os
 from setuptools import setup, find_packages
 
-version = "1.4.1"
+version = "1.5.0"
 
 def read(fname):
     return open(os.path.join(os.path.dirname(__file__), fname)).read()
