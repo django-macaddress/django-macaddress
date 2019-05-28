322ed63852bbddd8bc9fc031964a1823e48cee65
Bastien Vallet <b.vallet@criteo.com>
2019-05-28 12:20:04 +0200

    Add extra version testing for python3.7 and Django up to date versions

--- runtests.py
+++ runtests.py
@@ -5,26 +5,58 @@ import django
 from django.conf import settings
 
 
-if not settings.configured:
-    settings.configure(
-        DATABASES={
-            'default': {
-                'ENGINE': 'django.db.backends.sqlite3',
-                'NAME': ':memory:',
+SETTINGS = {
+    'DATABASES': {
+        'default': {
+            'ENGINE': 'django.db.backends.sqlite3',
+            'NAME': ':memory:',
+        }
+    },
+    'INSTALLED_APPS': (
+        'django.contrib.auth',
+        'django.contrib.contenttypes',
+        'django.contrib.sessions',
+        'django.contrib.messages',
+        'django.contrib.admin',
+        'macaddress.tests',
+        'macaddress',
+    ),
+    'SITE_ID': 1,
+    'SECRET_KEY': 'this-is-just-for-tests-so-not-that-secret',
+    'TEMPLATES': [
+        {
+            'BACKEND': 'django.template.backends.django.DjangoTemplates',
+            'APP_DIRS': True,
+            'OPTIONS': {
+                'context_processors': [
+                    'django.template.context_processors.debug',
+                    'django.template.context_processors.request',
+                    'django.contrib.auth.context_processors.auth',
+                    'django.contrib.messages.context_processors.messages',
+                ]
             }
-        },
-        INSTALLED_APPS=(
-            'django.contrib.auth',
-            'django.contrib.contenttypes',
-            'django.contrib.sessions',
-            'django.contrib.admin',
-            'macaddress.tests',
-            'macaddress',
-        ),
-        SITE_ID=1,
-        SECRET_KEY='this-is-just-for-tests-so-not-that-secret',
-        MIDDLEWARE_CLASSES=(),
+        }
+    ],
+}
+
+if django.VERSION < (2, 2):
+    SETTINGS['MIDDLEWARE_CLASSES'] = (
+        'django.contrib.auth.middleware.AuthenticationMiddleware',
+        'django.contrib.messages.middleware.MessageMiddleware',
+        'django.contrib.sessions.middleware.SessionMiddleware',
     )
+else:
+    SETTINGS['MIDDLEWARE'] = (
+        'django.contrib.auth.middleware.AuthenticationMiddleware',
+        'django.contrib.messages.middleware.MessageMiddleware',
+        'django.contrib.sessions.middleware.SessionMiddleware',
+    )
+
+
+
+if not settings.configured:
+    settings.configure(**SETTINGS)
+
 
 
 from django.test.utils import get_runner
